c3269e7a37ccf56a05e3ccba137ba7ff1eb8f4f4
APCS2nd <APCS2nd@MHS-Dreyer-01>
2018-10-22 09:49:00 -0700

    Incomplete Hourglass

---
+++ Hourglass/src/Hourglass.java
@@ -0,0 +1,30 @@
+/* Bryan Chan
+ * This program prints out a hourglass. 
+ * Version 0
+ * October 19, 2018
+ */
+import java.util.*;
+public class Hourglass {
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.println("Please enter width of hourglass: ");
+		int width = console.nextInt();
+	}
+	public static void base(int width) {
+		System.out.print("|");
+		for (int i = 1; i <= width; i++) {
+			System.out.print("\"");
+		}
+		System.out.print("|\n");
+	}
+	public static void tophalf(int width) {
+		String space = " ";
+		String character = ":";
+		int numoflines = 1;
+		while (numoflines <= ){
+			for (i)			
+			
+		}
+	}
+}
