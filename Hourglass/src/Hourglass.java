4a94b92ffd1959c26640ee7d61ce600571e32e04
APCS2nd <APCS2nd@MHS-Dreyer-01>
2018-10-24 11:29:50 -0700

    Completed tophalfmethod

--- Hourglass/src/Hourglass.java
+++ Hourglass/src/Hourglass.java
@@ -7,9 +7,15 @@ import java.util.*;
 public class Hourglass {
 
 	public static void main(String[] args) {
-		Scanner console = new Scanner(System.in);
-		System.out.println("Please enter width of hourglass: ");
-		int width = console.nextInt();
+		//Scanner console = new Scanner(System.in);
+		//System.out.println("Please enter width of hourglass (discounting borders ie \"|\"): ");
+		//int width = console.nextInt();
+		int width = 10;
+		base(width);
+		tophalf(width);
+		middle(width);
+		bottomhalf(width);
+		base(width);
 	}
 	public static void base(int width) {
 		System.out.print("|");
@@ -19,12 +25,22 @@ public class Hourglass {
 		System.out.print("|\n");
 	}
 	public static void tophalf(int width) {
-		String space = " ";
-		String character = ":";
-		int numoflines = 1;
-		while (numoflines <= ){
-			for (i)			
-			
+		for (int line = 1; line <= 4; line++) {
+			for (int i = 1; i <= line; i++) {		
+				System.out.print(" ");
+			}
+			System.out.print("\\");
+			for (int j = 1; j <= (-2 * (line) + 10); j++) {
+				System.out.print(":");
+			}
+			System.out.println("/");
 		}
 	}
+	public static void middle(int width) {
+		System.out.println("     ||");
+	}
+	public static void bottomhalf(int width) {
+		for (int i = 4; i >= 1; i--)
+	}
+	
 }
