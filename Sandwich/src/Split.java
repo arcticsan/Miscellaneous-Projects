bfbe63ab767662ccec7d8bec42cc933bb8f24b68
APCS2nd <APCS2nd@MHS-Dreyer-01>
2018-11-02 09:14:27 -0700

    Added Classwork

--- Sandwich/src/Split.java
+++ Sandwich/src/Split.java
@@ -4,43 +4,33 @@ public class Split {
 	public static void main(String[] args) {
 		// Your task Part 0
 
-		//String.split();
+				//String.split();
 
-		//It's a method that acts on a string, <StringName>.split(<sp>);
+				//It's a method that acts on a string, <StringName>.split(<String sp>);
 
-		//Where sp is the string where the string splits
+				//Where sp is the string where the string splits
 
-		//And it returns an array
+				//And it returns an array
 
-		// Example: "I like apples!".split(" ");
-		
-		//System.out.println(Arrays.toString("I like apples!".split(" ")));
-		
-		//String apples = "I like apples!";
-		
-		//System.out.println(Arrays.toString(apples.split(" ")));
+				// Example: "I like apples!".split(" ");
+				//String[] apples = "I like apples!".split(" ");
+				System.out.println(Arrays.toString("I like apples!".split(" ")));
+				
 
-		// it will split at spaces and return an array of ["I","like","apples!"]
+				// it will split at spaces and return an array of ["I","like","apples!"]
 
-		// Example 2: "I really like really red apples!".split("really")
+				// Example 2: "I really like really red apples"split("really")
+				System.out.println(Arrays.toString("I really like reallyreally red apples!".split("really")));
+				String[] applesArray = "I really like reallyreally red apples".split("really");
+				System.out.println(Arrays.toString(applesArray));
+				System.out.println(applesArray[2]);
 
-		// it will split at the word "really" and return an array of ["I "," like ","red apples!"]
+				// it will split at the word "really" and return an array of ["I "," like ","red apples!"]
 
-		//play around with String.split!
+				//play around with String.split!
 
-		//What happens if you "I reallyreally likeapples".split("really") ?
-		
-		//Part 1
-		String food = "applespineapplesbreadlettucetomatobreadbaconmayohambreadcheese";
-		//food = "cheesebreadbaconlettuceapples";
-		
-		food = "applespineapplesbreadlettucetomatobaconmayohambreadcheese";
-		
-		System.out.println(sandwichDescriber(food));
-		
-		//Part 2
-		System.out.println(sandwichDescriberPt2("apples pineapples bread lettuce tomato bacon mayo ham bread cheese"));
-		
+				//What happens if you "I reallyreally likeapples".split("really") ?
+				System.out.println(Arrays.toString("I reallyreally likeapples".split("really")));
 	}
 	//Your task Part 1:
 
