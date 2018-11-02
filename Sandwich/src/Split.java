0481f7301843fba59b545851d2090e73a9ce9471
APCS2nd <APCS2nd@MHS-Dreyer-01>
2018-11-02 09:50:34 -0700

    Continue Optimizing Split.java

--- Sandwich/src/Split.java
+++ Sandwich/src/Split.java
@@ -4,33 +4,41 @@ public class Split {
 	public static void main(String[] args) {
 		// Your task Part 0
 
-				//String.split();
+			//String.split();
 
-				//It's a method that acts on a string, <StringName>.split(<String sp>);
+			//It's a method that acts on a string, <StringName>.split(<String sp>);
 
-				//Where sp is the string where the string splits
+			//Where sp is the string where the string splits
 
-				//And it returns an array
+			//And it returns an array
 
-				// Example: "I like apples!".split(" ");
-				//String[] apples = "I like apples!".split(" ");
-				System.out.println(Arrays.toString("I like apples!".split(" ")));
-				
+			// Example: "I like apples!".split(" ");
+			//String[] apples = "I like apples!".split(" ");
+			System.out.println(Arrays.toString("I like apples!".split(" ")));
+	
+			// it will split at spaces and return an array of ["I","like","apples!"]
 
-				// it will split at spaces and return an array of ["I","like","apples!"]
+			// Example 2: "I really like really red apples"split("really")
+			System.out.println(Arrays.toString("I really like reallyreally red apples!".split("really")));
+			String[] applesArray = "I really like reallyreally red apples".split("really");
+			System.out.println(Arrays.toString(applesArray));
+			System.out.println(applesArray[2]);
+			// it will split at the word "really" and return an array of ["I "," like ","red apples!"]
+			//play around with String.split!
 
-				// Example 2: "I really like really red apples"split("really")
-				System.out.println(Arrays.toString("I really like reallyreally red apples!".split("really")));
-				String[] applesArray = "I really like reallyreally red apples".split("really");
-				System.out.println(Arrays.toString(applesArray));
-				System.out.println(applesArray[2]);
-
-				// it will split at the word "really" and return an array of ["I "," like ","red apples!"]
-
-				//play around with String.split!
-
-				//What happens if you "I reallyreally likeapples".split("really") ?
-				System.out.println(Arrays.toString("I reallyreally likeapples".split("really")));
+			//What happens if you "I reallyreally likeapples".split("really") ?
+			System.out.println(Arrays.toString("I reallyreally likeapples".split("really")));
+			
+			//Part 1
+			String food = "applespineapplesbreadlettucetomatobreadbaconmayohambreadcheese";
+			//food = "cheesebreadbaconlettuceapples";
+			
+			food = "applespineapplesbreadlettucetomatobaconmayohambreadcheese";
+			
+			System.out.println(sandwichDescriber(food));
+			
+			//Part 2
+			System.out.println(sandwichDescriberPt2("apples pineapples bread lettuce tomato bacon mayo ham bread cheese"));
 	}
 	//Your task Part 1:
 
@@ -50,18 +58,10 @@ public class Split {
 		
 		public static String sandwichDescriber(String food) {
 			int FirstBread_Index = food.indexOf("bread");
-		
-			//System.out.println(FirstBread_Index);
-		
 			int Last_BreadIndex = FirstBread_Index + 1;
-		
-			//System.out.println(food.indexOf("bread", Last_BreadIndex));
-			
 			if (FirstBread_Index == -1) return("");
-		
 			boolean lastBread = false;
-		
-			do {
+			while (lastBread == false) {
 				if (food.indexOf("bread",Last_BreadIndex) != -1) {
 					lastBread = false;
 					Last_BreadIndex++;
@@ -73,15 +73,9 @@ public class Split {
 					System.out.println(Last_BreadIndex);
 				}
 			}
-			while (lastBread == false);
-		
 			food = food.substring(FirstBread_Index,(Last_BreadIndex));
-		
-			//System.out.println("food is " + food);
-				
 			return (Arrays.toString(food.split("bread")));
 		}
-		
 
 		//Your task pt 2:
 
